f463a8a4c25341fca6699fdd411c28a1b867fad6
Suvarchala04 <gadesuvarchala96@gmail.com>
2024-05-30 20:33:57 +0530

    Initial commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 4000;
 
 const numbers = [];
 const windowSize = 10;
-const baseUrl = 'http://20.244.56.144/test';
+const url = 'http://20.244.56.144/test';
 
 const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE3MDgwMTE4LCJpYXQiOjE3MTcwNzk4MTgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImVjMDIwZmU1LWE4N2QtNDI4YS1hMTUyLWNjZDFhODVhNmNhMCIsInN1YiI6ImdhZGVzdXZhcmNoYWxhOTZAZ21haWwuY29tIn0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiJlYzAyMGZlNS1hODdkLTQyOGEtYTE1Mi1jY2QxYTg1YTZjYTAiLCJjbGllbnRTZWNyZXQiOiJWZHV2aW52dElxSVJ1bkRUIiwib3duZXJOYW1lIjoiU3V2YXJjaGFsYSIsIm93bmVyRW1haWwiOiJnYWRlc3V2YXJjaGFsYTk2QGdtYWlsLmNvbSIsInJvbGxObyI6IjIxQkQxQTA1QUcifQ.ucXgCkau79tCD7cdpJ3ZRAX5WAGPzQSB2ewfaqeNEU4';
 
@@ -29,16 +29,16 @@ let nums = [];
 app.get('/numbers/:id', async (req, res) => {
     let { id } = req.params;
     if (id === 'e') {
-        let newUrl = `${baseUrl}/even`;
+        let newUrl = `${url}/even`;
         nums = await getData(newUrl);
     } else if (id === 'p') {
-        let newUrl = `${baseUrl}/prime`;
+        let newUrl = `${url}/prime`;
         nums = await getData(newUrl);
     } else if (id === 'f') {
-        let newUrl = `${baseUrl}/fibo`;
+        let newUrl = `${url}/fibo`;
         nums = await getData(newUrl);
     } else if (id === 'r') {
-        let newUrl = `${baseUrl}/rand`;
+        let newUrl = `${url}/rand`;
         nums = await getData(newUrl);
     }
 
